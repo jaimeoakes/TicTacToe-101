a76f896049c512d0b1bb23c1060b1d4e5ced8c18
Jaime Oakes <oakesjaime@gmail.com>
2024-04-09 22:27:17 -0500

    javaScript

--- scripts.js
+++ scripts.js
@@ -17,11 +17,11 @@ let currentMarker = 'X'
 const handleClick = (element) => {
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
+console.log(`The element you clicked on has an id:  ${element.id}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
+if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
 }
@@ -40,8 +40,8 @@ const handleClick = (element) => {
 const addMarker = (id) => {
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
-  console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+console.log(`*** The current marker is:  ${currentMarker}. ***`)
+console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
   
