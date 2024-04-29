ba123170280fadf2f8cfe7abcdbe64d93e767a98
Jaime Oakes <oakesjaime@gmail.com>
2024-04-29 16:58:11 -0500

    adding things

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
 
+let board = [["","",""],["","",""],["","",""]]
 
 
 
@@ -17,11 +18,11 @@ let currentMarker = 'X'
 const handleClick = (element) => {
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-console.log(`The element you clicked on has an id:  ${element.id}`)
+  console.log(`The element you clicked on has an id:  ${element.id}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-if(!document.getElementById(element.id).innerHTML){
+  if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
 }
@@ -40,18 +41,27 @@ if(!document.getElementById(element.id).innerHTML){
 const addMarker = (id) => {
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
-console.log(`*** The current marker is:  ${currentMarker}. ***`)
-console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+  console.log(`*** The current marker is:  ${currentMarker}. ***`)
+  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
   
+  document.getElementById(id).innerHTML = currentMarker;
+
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+
+
+  board[row][column] = currentMarker
+  console.log(board)
+
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
 
-  changeMarker()
+  checkForWin()
 }
 
 
@@ -87,6 +97,8 @@ const resetBoard = () => {
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
+  const squares = document.getElementsByTagName("TD");
+
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
@@ -103,4 +115,32 @@ const resetBoard = () => {
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
+}
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") ||
+  (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") ||
+  (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") ||
+  (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") ||
+  (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") ) {
+  return true
+}
+}
+
+const verticalWin = () => {
+  // Your code here to check for vertical wins
+}
+
+const diagonalWin = () => {
+  // Your code here to check for diagonal wins
 }
